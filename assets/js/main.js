8ccffaa751d5cd9fb21dd669b0a0049734090795
carminenicotera <carmine.91.n@hotmail.it>
2026-02-25 16:24:34 +0100

    complete third milestone

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,28 @@
 const endpoint = "https://lanciweb.github.io/demo/api/pictures/"
 console.log(endpoint);
+
+const rowEl = document.querySelector('.row')
+console.log(rowEl);
+
+fetch(endpoint)
+  .then(result => result.json())
+  .then(data => {
+    console.log(data);
+
+    data.forEach(element => {
+      console.log(element);
+      const { title, date, url } = element;
+      const markup = `<div class="col">
+            <div class="card">
+              <img src="./assets/img/pin.svg" alt="pin" class="pin">
+              <div class="card-body">
+                <img src=${url} alt="img" class="card-img-top">
+                <h3>${date}</h3>
+                <div>${title}</div>
+              </div>
+            </div>
+          </div>`
+      console.log(markup);
+      rowEl.innerHTML += markup;
+    });
+  })
